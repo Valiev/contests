7ade037d662ca45b56145519d6ad1d298e42c544
Sergey Valiev <svaliev@gmail.com>
2023-12-24 21:51:16 +0300

    leetcode: 21_Merge_Two_Sorted_Lists no recursion

--- leetcode/21_Merge_Two_Sorted_Lists.py
+++ leetcode/21_Merge_Two_Sorted_Lists.py
@@ -3,18 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        node = list1
-        next_l1 = list1.next
-        next_l2 = list2
-        if list1.val > list2.val:
-            node = list2
-            next_l1 = list1
-            next_l2 = list2.next
-        node.next = self.mergeTwoLists(next_l1, next_l2)
-        return node
+        fake_head = ListNode()
+        head = fake_head
+        while list1 or list2:
+            if list1 is None:
+                head.next = list2
+                break
+            if list2 is None:
+                head.next = list1
+                break
+            v1, v2 = list1.val, list2.val
+            if v1 <= v2:
+                head.next = list1
+                head = head.next
+                list1 = list1.next
+            else:
+                head.next = list2
+                head = head.next
+                list2 = list2.next
+        return fake_head.next
