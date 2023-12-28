5e62d4a89f84d8169b4ab75d38bc8823348b5812
Sergey Valiev <svaliev@gmail.com>
2023-12-28 19:22:15 +0300

    leecode: 102_Binary_Tree_Level_Order_Traversal (again)

--- leetcode/102_Binary_Tree_Level_Order_Traversal.py
+++ leetcode/102_Binary_Tree_Level_Order_Traversal.py
@@ -22,3 +22,30 @@ class Solution:
                 stack.append((lvl+1, node.left))
         return levels
 
+
+class Solution2:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return None
+
+        def next_level(level):
+            acc = []
+            for node in level:
+                if node is None:
+                    continue
+                if node.left is not None:
+                    acc.append(node.left)
+                if node.right is not None:
+                    acc.append(node.right)
+            return acc
+
+        acc = []
+        stack = [[root]]
+        while stack:
+            level = stack.pop()
+            if not level:
+                break
+            acc.append([node.val for node in level])
+            stack.append(next_level(level))
+        return acc
+
