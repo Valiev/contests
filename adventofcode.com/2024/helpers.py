f77f5e6de6f085954004946463dde3feb6e40a0f
Sergey Valiev <svaliev@joom.com>
2024-12-04 22:39:27 +0300

    adventofcode: helpers new classes

--- adventofcode.com/2024/helpers.py
+++ adventofcode.com/2024/helpers.py
@@ -6,3 +6,22 @@ INPUT_FOLDER = Path(__file__).parent / "inputs"
 def input_filepath(filename):
     return (INPUT_FOLDER / filename).resolve()
 
+
+class Result:
+    def __init__(self, value):
+        self.value = value
+
+    def is_ok(self):
+        return not self.is_error()
+
+    def is_error(self):
+        return not self.is_ok()
+
+class Ok(Result):
+    def is_ok(self):
+        return True
+
+
+class Error(Result):
+    def is_error(self):
+        return True
