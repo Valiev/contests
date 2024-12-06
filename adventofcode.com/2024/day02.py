7e06578990a491e37418e29f5b758c939921c3a1
Sergey Valiev <svaliev@joom.com>
2024-12-06 22:57:28 +0300

    day02: complete

--- adventofcode.com/2024/day02.py
+++ adventofcode.com/2024/day02.py
@@ -44,6 +44,29 @@ def count_safe_reports(filename):
                 total += 1
     return total
 
+def count_safe_reports2(filename):
+    total = 0
+    with open(filename) as fp:
+        for line in fp:
+            if not line.strip():
+                continue
+
+            nums = [int(i) for i in line.strip().split()]
+            result = safe_report(nums)
+            if result.is_ok():
+                total += 1
+                continue
+
+            for pos in range(len(nums)):
+                value = nums.pop(pos)
+                if safe_report(nums).is_ok():
+                    total += 1
+                    break
+                nums.insert(pos, value)
+
+    return total
+
 if __name__ == "__main__":
     input_file = input_filepath("day02.txt")
     print(count_safe_reports(input_file))
+    print(count_safe_reports2(input_file))
