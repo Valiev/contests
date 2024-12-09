9dc5e13e8f29a51625e69bf4a2823da473dda50d
Sergey Valiev <svaliev@joom.com>
2024-12-09 18:59:01 +0300

    advent: day05 part2 (not working)

--- adventofcode.com/2024/day05.py
+++ adventofcode.com/2024/day05.py
@@ -1,4 +1,5 @@
 from helpers import input_filepath
+from pprint import pprint
 
 example = """\
 47|53
@@ -76,7 +77,37 @@ def check_seq(graph, seq):
     return True
 
 
-def puzzle1(filename):
+def cmp_graph_pos(graph, from_node, to_node):
+    def dfs(n1, n2):
+        if n1 not in graph:
+            return False
+        if n2 in graph[n1]:
+            return True
+        return any(
+            dfs(k, n2)
+            for k in graph[n1]
+        )
+    return dfs(from_node, to_node)
+
+
+def fix_seq(graph, seq):
+    N = len(seq)
+    print('fixing seq', seq)
+    for pos in range(N):
+        print('- pos:', pos)
+        cmp_pos = pos + 1
+        while cmp_pos < N:
+            print(cmp_pos)
+            if cmp_graph_pos(graph, seq[pos], seq[cmp_pos]):
+                cmp_pos += 1
+            else:
+                print(seq, "switching", seq[pos], seq[cmp_pos])
+                seq[pos], seq[cmp_pos] = seq[cmp_pos], seq[pos]
+                cmp_pos = pos + 1
+    return seq
+
+
+def prep_data(filename):
     with open(filename) as fp:
         input_data = fp.read()
     graph_data, seq_data = input_data.split('\n\n')
@@ -85,6 +116,11 @@ def puzzle1(filename):
         [int(i) for i in line.split(',')]
         for line in seq_data.splitlines()
     ]
+    return graph, seqs
+
+
+def puzzle1(filename):
+    graph, seqs = prep_data(filename)
     return sum(
         seq[len(seq)//2]
         for seq in seqs
@@ -92,7 +128,19 @@ def puzzle1(filename):
     )
 
 
+def puzzle2(filename):
+    graph, seqs = prep_data(filename)
+    # pprint(graph)
+    return sum(
+        fix_seq(graph, seq)[len(seq)//2]
+        for seq in seqs
+        if not check_seq(graph, seq)
+    )
+
+
 if __name__ == "__main__":
     input_file = input_filepath("day05.txt")
     result1 = puzzle1(input_file)
     print(result1)
+    result2 = puzzle2(input_file)
+    print(result2)
