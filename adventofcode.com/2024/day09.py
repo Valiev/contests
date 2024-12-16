cca9ee6bcc53c494a7665c37283c855f7382cde6
Sergey Valiev <svaliev@joom.com>
2024-12-16 22:29:02 +0300

    advent: day09 part 2

--- adventofcode.com/2024/day09.py
+++ adventofcode.com/2024/day09.py
@@ -1,4 +1,6 @@
 from helpers import BasePuzzle
+from collections import defaultdict
+from itertools import batched
 
 
 class Puzzle(BasePuzzle):
@@ -27,8 +29,7 @@ class Puzzle(BasePuzzle):
             checksum += idx * elem
         return checksum
 
-    def solve1(self):
-        filesystem = self.get_filesystem()
+    def fs_format1(self, filesystem):
         left, right = 0, len(filesystem) - 1
         while left < right:
             L = filesystem[left]
@@ -43,9 +44,71 @@ class Puzzle(BasePuzzle):
 
             filesystem[left] = R
             filesystem[right] = L
+
+    def fs_freespaces(self, filesystem):
+        stack = []
+        for idx, elem in enumerate(filesystem):
+            if elem == '.':
+                stack.append(idx)
+                continue
+
+            if not stack:
+                continue
+
+            yield stack[0], len(stack)
+            while stack:
+                stack.pop()
+
+        if stack:
+            yield stack[0], len(stack)
+
+    def fs_format2(self, filesystem):
+        file_sizes = defaultdict(int)
+        file_poses = dict()
+        max_file_id = None
+        for idx, elem in enumerate(filesystem):
+            if elem == '.':
+                continue
+            file_sizes[elem] += 1
+            if elem not in file_poses:
+                file_poses[elem] = idx
+            if max_file_id is None:
+                max_file_id = elem
+            max_file_id = max(max_file_id, elem)
+
+        for file_id in range(max_file_id, -1, -1):
+            file_size = file_sizes[file_id]
+            print(f'Formatting {file_id} file with {file_size} len')
+            file_pos = file_poses[file_id]
+            for free_pos, fs_len in self.fs_freespaces(filesystem):
+                if free_pos >= file_pos:
+                    break
+                if file_size > fs_len:
+                    continue
+                print(f'- Switching with free_space {fs_len} len')
+                for i in range(file_size):
+                    filesystem[file_pos+i], filesystem[free_pos+i] = filesystem[free_pos+i], filesystem[file_pos+i]
+                break
+            else:
+                print(f'- No switch for {file_id}')
+
+        return filesystem
+
+    def solve(self, format_function):
+        filesystem = self.get_filesystem()
+        format_function(filesystem)
+        for batch in batched(filesystem, 10):
+            print(' '.join(str(i) for i in batch))
         return self.fs_checksum(filesystem)
+
+    def solve1(self):
+        return self.solve(self.fs_format1)
+
+    def solve2(self):
+        return self.solve(self.fs_format2)
 
 
 if __name__ == "__main__":
     puzzle = Puzzle()
     print(puzzle.solve1())
+    print(puzzle.solve2())
